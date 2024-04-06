8d12cc6dae8bca86d35115ec7f16aa6a0c1d6e0b
Mykhailo Bondarenko <mcmikius@icloud.com>
2024-04-06 13:35:21 +0300

    feat: add openapi spec

--- Package.swift
+++ Package.swift
@@ -1,23 +1,38 @@
-// swift-tools-version: 5.10
+// swift-tools-version: 5.9
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
-    name: "FSITunesSearchClient",
+    name: "fs-itunes-search-client",
+    platforms: [
+        .iOS(.v16),
+        .macOS(.v13)
+    ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
-            name: "FSITunesSearchClient",
-            targets: ["FSITunesSearchClient"]),
+            name: "ITunesSearchClient",
+            targets: ["ITunesSearchClient"]),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/apple/swift-openapi-generator", from: "1.0.0"),
+        .package(url: "https://github.com/apple/swift-openapi-runtime", from: "1.0.0"),
+        .package(url: "https://github.com/apple/swift-openapi-urlsession", from: "1.0.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "FSITunesSearchClient"),
+            name: "ITunesSearchClient", dependencies: [
+                .product(name: "OpenAPIRuntime", package: "swift-openapi-runtime"),
+                .product(name: "OpenAPIURLSession", package: "swift-openapi-urlsession"),
+            ],
+            plugins: [
+                .plugin(name: "OpenAPIGenerator", package: "swift-openapi-generator")
+            ]),
         .testTarget(
-            name: "FSITunesSearchClientTests",
-            dependencies: ["FSITunesSearchClient"]),
+            name: "ITunesSearchClientTests",
+            dependencies: ["ITunesSearchClient"]),
     ]
 )
--- Tests/ITunesSearchClientTests/FSITunesSearchClientTests.swift
+++ Tests/ITunesSearchClientTests/FSITunesSearchClientTests.swift
@@ -1,7 +1,7 @@
 import XCTest
-@testable import FSITunesSearchClient
+@testable import ITunesSearchClient
 
-final class FSITunesSearchClientTests: XCTestCase {
+final class ITunesSearchClientTests: XCTestCase {
     func testExample() throws {
         // XCTest Documentation
         // https://developer.apple.com/documentation/xctest
