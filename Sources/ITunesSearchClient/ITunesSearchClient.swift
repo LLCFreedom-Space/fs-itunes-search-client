9245fe525edeb9232c4173df6e74d1e4e159cd3e
Mykhailo Bondarenko <mcmikius@icloud.com>
2024-04-09 23:47:24 +0300

    test: add tests

--- Sources/ITunesSearchClient/ITunesSearchClient.swift
+++ Sources/ITunesSearchClient/ITunesSearchClient.swift
@@ -3,11 +3,16 @@ import Foundation
 import OpenAPIURLSession
 
 public protocol ITunesSearchClientProtocol {
-    func fetchBundleIdentifiers(bundleId: String) async throws -> [String]
+    func fetchBundleIdentifiers(by bundleId: String) async throws -> [String]
+    func get(by bundleId: String, _ countryCode: String?) async throws -> AppInfo
 }
 
 public struct ITunesSearchClient: ITunesSearchClientProtocol {
-    private let client: Client
+    private let client: any APIProtocol
+    
+    init(client: any APIProtocol) {
+        self.client = client
+    }
     
     public init() throws {
         do {
@@ -16,11 +21,44 @@ public struct ITunesSearchClient: ITunesSearchClientProtocol {
                 transport: URLSessionTransport()
             )
         } catch {
-            throw ITunesSearchClientError.failedToDecode
+            throw ITunesSearchClientError.failedConfigClient
         }
     }
     
-    public func fetchBundleIdentifiers(bundleId: String) async throws -> [String] {
+    public func get(by bundleId: String, _ countryCode: String? = nil) async throws -> AppInfo {
+        let query = Operations.getByBundleId.Input.Query(bundleId: bundleId, country: countryCode)
+        let result = try await client.getByBundleId(query: query)
+        switch result {
+        case .ok(let response):
+            switch response.body {
+            case .json(let json):
+                guard json.results?.count == 1,
+                      let result = json.results?.first(where: {$0.bundleId == bundleId}) else {
+                    throw ITunesSearchClientError.invalidResponse
+                }
+                return AppInfo(result)
+            case .text_javascript(let body):
+                let bytes = 12000
+                guard let jsonData = try await String(collecting: body, upTo: bytes).data(using: .utf8) else {
+                    throw ITunesSearchClientError.failedToGetResponseData
+                }
+                do {
+                    let appResponse = try JSONDecoder().decode(Components.Schemas.AppResponse.self, from: jsonData)
+                    guard appResponse.results?.count == 1,
+                          let result = appResponse.results?.first(where: {$0.bundleId == bundleId}) else {
+                        throw ITunesSearchClientError.invalidResponse
+                    }
+                    return AppInfo(result)
+                } catch {
+                    throw ITunesSearchClientError.failedToDecode
+                }
+            }
+        case .default(let statusCode, _):
+            throw ITunesSearchClientError.serverError(errorCode: statusCode)
+        }
+    }
+    
+    public func fetchBundleIdentifiers(by bundleId: String) async throws -> [String] {
         let query = Operations.getByBundleId.Input.Query(bundleId: bundleId)
         let result = try await client.getByBundleId(query: query)
         switch result {
@@ -32,8 +70,9 @@ public struct ITunesSearchClient: ITunesSearchClientProtocol {
                 }
                 return result
             case .text_javascript(let body):
-                guard let jsonData = try await String(collecting: body, upTo: 12000).data(using: .utf8) else {
-                    throw ITunesSearchClientError.failedToDecode
+                let bytes = 12000
+                guard let jsonData = try await String(collecting: body, upTo: bytes).data(using: .utf8) else {
+                    throw ITunesSearchClientError.failedToGetResponseData
                 }
                 do {
                     let res = try JSONDecoder().decode(Components.Schemas.AppResponse.self, from: jsonData)
@@ -55,4 +94,21 @@ public enum ITunesSearchClientError: Error {
     case serverError(errorCode: Int)
     case failedToDecode
     case emptyResult
+    case failedToGetResponseData
+    case invalidResponse
+    case failedConfigClient
 }
+
+public struct AppInfo {
+    public let version: String?
+    public let bundleId: String?
+    public let currentVersionReleaseDate: String?
+    
+    init(_ result: Components.Schemas.Result) {
+        self.version = result.version
+        self.bundleId = result.bundleId
+        self.currentVersionReleaseDate = result.currentVersionReleaseDate
+    }
+}
+
+
--- Tests/ITunesSearchClientTests/ITunesSearchClientIntegrationTests.swift
+++ Tests/ITunesSearchClientTests/ITunesSearchClientIntegrationTests.swift
@@ -9,9 +9,17 @@ import XCTest
 @testable import ITunesSearchClient
 
 final class ITunesSearchClientIntegrationTests: XCTestCase {
-    func testFetchBundleIDs() async throws {
+    func testFetchBundleIdentifiersetchBundleIDs() async throws {
         let client = try ITunesSearchClient()
-        let bundleIDs = try await client.fetchBundleIdentifiers(bundleId: "com.apple.Pages")
+        let bundleIDs = try await client.fetchBundleIdentifiers(by: "com.apple.Pages")
         XCTAssertEqual(["com.apple.Pages"], bundleIDs)
+    }
+    
+    func testByBundleId() async throws {
+        let client = try ITunesSearchClient()
+        let result = try await client.get(by: "com.apple.Keynote")
+        XCTAssertEqual(result.bundleId, "com.apple.Keynote")
+        XCTAssertNotNil(result.version)
+        XCTAssertNotNil(result.currentVersionReleaseDate)
     }
 }
